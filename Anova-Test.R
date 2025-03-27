f15d60b1b47fa1f91aec476ef592942af93486e3
Annyya Bryant <annbryant@claflin.edu>
2025-03-27 10:10:01 -0400

    Add files via upload

---
+++ Anova-Test.R
@@ -0,0 +1,14 @@
+#Author: A'nnyya Bryant; Date: 03/26/2025; Purpose: ANOVA Test.
+
+#Load a library dplyr
+library(dplyr)
+
+#Store the http link in a variable called PATH
+PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poisons.csv"
+
+#Read the CSV file from above link
+df <- read.table(PATH,row.names=NULL)
+
+
+
+
